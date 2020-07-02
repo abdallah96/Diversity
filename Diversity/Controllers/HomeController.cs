8cdc04ef7501793d5c7b66d96baff0032d7a4c31
Abdallah Gueye <abdallaha.gueye@hotmail.com>
2020-07-02 22:32:00 +0300

    Add project files.

---
+++ Diversity/Controllers/HomeController.cs
@@ -0,0 +1,199 @@
+﻿using AirtableApiClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Nito.AsyncEx;
+using Nito.AsyncEx.Synchronous;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+namespace Diversity.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        static string AirtableApiKey = "keylaGU2v6mUyG56F";
+        static string AirtableBaseId = "appOPEcVghry00Fuq";
+
+        AirtableBase airtableDb = new AirtableBase(AirtableApiKey, AirtableBaseId);
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private static string[] employeeFields = { "Age", "LGBTQIA", "HasDisability", "ServeMilitary", "Gender", "Ethnicity", "FamilyStatus", "Location", "Language" };
+        private List<Dictionary<string, object>> GetAirEmployeeList()
+        {
+            var allEmployees = new List<AirtableRecord>();
+            string offset = null, errorMessage = null;
+
+            do
+            {
+                var task = Task.Run(async () => await airtableDb.ListRecords("Employee", offset, employeeFields ));
+                AirtableListRecordsResponse response = task.WaitAndUnwrapException();
+
+                if(response.Success)
+                {
+                    allEmployees.AddRange(response.Records.ToList());
+                    offset = response.Offset;
+                }
+                else if (response.AirtableApiError is AirtableApiException)
+                {
+                    errorMessage = response.AirtableApiError.ErrorMessage;
+                    break;
+                }
+                else
+                {
+                    errorMessage = "Unknown error";
+                    break;
+                }
+            }
+            while (offset != null);
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                return null;
+            }
+            else
+            {
+                var result = allEmployees.Select(emp => emp.Fields).ToList();
+                return result;
+            }
+        }
+
+        private List<string> GetAirEthnicitiesList()
+        {
+            var allEthnicities = new List<AirtableRecord>();
+            string offset = null, errorMessage = null;
+
+            do
+            {
+                var task = Task.Run(async () => await airtableDb.ListRecords("Ethnicity"));
+                AirtableListRecordsResponse response = task.WaitAndUnwrapException();
+
+                if (response.Success)
+                {
+                    allEthnicities.AddRange(response.Records.ToList());
+                    offset = response.Offset;
+                }
+                else if (response.AirtableApiError is AirtableApiException)
+                {
+                    errorMessage = response.AirtableApiError.ErrorMessage;
+                    break;
+                }
+                else
+                {
+                    errorMessage = "Unknown error";
+                    break;
+                }
+            }
+            while (offset != null);
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                return null;
+            }
+            else
+            {
+                var result = allEthnicities.Select(emp => emp.Fields).Select(field => (string)field["Name"]).ToList();
+                return result;
+            }
+        }
+
+        public string GetAirEmployees()
+        {
+            var employees = GetAirEmployeeList();
+            if (employees == null)
+            {
+                return "List of employees could not be retrieved";
+            }
+            return JsonConvert.SerializeObject(employees);
+        }
+
+        public string GetPercentages() 
+        {
+            var employees = GetAirEmployeeList();
+            if(employees == null)
+            {
+                return "List of employees could not be retrieved";
+            }
+            var noOfEmployees = employees.Count();
+            var percentages = new
+            {
+                lqbgqia = (float)(employees.Where(emp => (string)emp["LGBTQIA"] == "checked").Count()) / noOfEmployees,
+                disability = (float)(employees.Where(emp => (string)emp["HasDisability"] == "checked").Count()) / noOfEmployees,
+                military = (float)(employees.Where(emp => (string)emp["ServeMilitary"] == "checked").Count()) / noOfEmployees,
+                language = (float) (employees.Where(emp => ((JArray)emp["Language"]).Count()>0)).Count()/noOfEmployees,
+            };
+
+            return JsonConvert.SerializeObject(percentages);
+        }
+
+        public string GetNumbers()
+        {
+            var ethnicities = GetAirEthnicitiesList();
+            var employees = GetAirEmployeeList();
+            if (employees == null)
+            {
+                return "List of employees could not be retrieved";
+            }
+            var numbers = new
+            {
+                gender = new List<Object>
+                {
+                    new { name = "male", value = (float)(employees.Where(emp => (String)emp["Gender"] == "Male").Count()) / employees.Count() },
+                    new { name = "female", value = (float)(employees.Where(emp => (String)emp["Gender"] == "Female").Count()) / employees.Count() },
+                    new { name = "other", value = (float)(employees.Where(emp => (String)emp["Gender"] == "Agender").Count()) / employees.Count() }
+
+                },
+
+                ethnicity = new List<Object>(),
+                location = new List<Object>
+                {
+                    new { name = "NorthAmerica", value = (float)(employees.Where(emp => (String)emp["Location"] == "NorthAmerica").Count()) / employees.Count() },
+                    new { name = "Europe", value = (float)(employees.Where(emp => (String)emp["Location"] == "Europe").Count()) / employees.Count() },
+                    new { name = "Asia", value = (float)(employees.Where(emp => (String)emp["Location"] == "Asia").Count()) / employees.Count() },
+                    new { name = "Noma", value = (float)(employees.Where(emp => (String)emp["Location"] == "Noma").Count()) / employees.Count() },
+                    new { name = "Australasia", value = (float)(employees.Where(emp => (String)emp["Location"] == "Australasia").Count()) / employees.Count() },
+                    new { name = "SouthAmerica", value = (float)(employees.Where(emp => (String)emp["Location"] == "CentralorSouthAmerica").Count()) / employees.Count() },
+                    new { name = "Africa", value = (float)(employees.Where(emp => (String)emp["Location"] == "Africa").Count()) / employees.Count() },
+
+                },
+
+                familyStatus = new List<Object>
+                {
+                    new { name = "Partnered", value = (float)(employees.Where(emp => (String)emp["FamilyStatus"] == "Partnered Parent/Legal Garant").Count()) / employees.Count() }, 
+                    new { name = "No Children", value = (float)(employees.Where(emp => (String)emp["FamilyStatus"] == "No Children").Count()) / employees.Count() },
+                    new { name = "Unknown", value = (float)(employees.Where(emp => (String)emp["FamilyStatus"] == "Unknown").Count()) / employees.Count() },
+                },
+            
+                age = new List<Object>
+                {
+                    new { name = "[0, 18]", value = (float)(employees.Where(emp => Int32.Parse((string)emp["Age"]) > 0 && Int32.Parse((string)emp["Age"]) <= 18).Count()) / employees.Count() },
+                    new { name = "[18, 25]", value = (float)(employees.Where(emp => Int32.Parse((string)emp["Age"]) > 18 && Int32.Parse((string)emp["Age"]) <= 25).Count()) / employees.Count() },
+                    new { name = "[35, 45]", value = (float)(employees.Where(emp => Int32.Parse((string)emp["Age"]) > 35 && Int32.Parse((string)emp["Age"]) <= 45).Count()) / employees.Count() },
+                    new { name = "[45, 55]", value = (float)(employees.Where(emp => Int32.Parse((string)emp["Age"]) > 45 && Int32.Parse((string)emp["Age"]) <= 55).Count()) / employees.Count() },
+                    new { name = "[55, 65]", value = (float)(employees.Where(emp => Int32.Parse((string)emp["Age"]) > 55 && Int32.Parse((string)emp["Age"]) <= 65).Count()) / employees.Count() },
+                    new { name = "[65, Inf]", value = (float)(employees.Where(emp => Int32.Parse((string)emp["Age"]) > 65).Count()) / employees.Count() },
+                }
+            };
+
+           
+            for (int i=0; i<ethnicities.Count(); i++)
+            {
+                numbers.ethnicity.Add(new {
+                    name = ethnicities[i],
+                    value = (float)(employees.Where(emp => (String)emp["Ethnicity"] == ethnicities[i]).Count()) / employees.Count()
+                });
+            }
+
+            return JsonConvert.SerializeObject(numbers);
+        }
+
+
+    }
+}
---
+++ Diversity/Scripts/Diversity/diversity.js
@@ -0,0 +1,189 @@
+﻿Diversity.controller('HomeCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
+
+    $scope.percentage = [];
+    $scope.number = [];
+
+    $scope.init = function () {
+        $scope.GetPercentages();
+        $scope.GetNumbers();
+        $scope.GetEmployees();
+    };
+
+    function generateBarChart(data, elementId) {
+        //sort bars based on value
+        data = data.sort(function (a, b) {
+            return d3.ascending(a.value, b.value);
+        })
+
+        //set up svg using margin conventions - we'll need plenty of room on the left for labels
+        var margin = {
+            top: 15,
+            right: 25,
+            bottom: 15,
+            left: 120
+        };
+
+        var width = 350 - margin.left - margin.right,
+            height = 200 - margin.top - margin.bottom;
+
+        var svg = d3.select("#" + elementId).append("svg")
+            .attr("width", width + margin.left + margin.right + 10)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scale.linear()
+            .range([0, width])
+            .domain([0, d3.max(data, function (d) {
+                return d.value;
+            })]);
+
+        var y = d3.scale.ordinal()
+            .rangeRoundBands([height, 0], .1)
+            .domain(data.map(function (d) {
+                return d.name;
+            }));
+
+        //make y axis to show bar names
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            //no tick marks
+            .tickSize(0)
+            .orient("left");
+
+        var gy = svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+
+        var bars = svg.selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("g")
+
+        //append rects
+        bars.append("rect")
+            .attr("class", "bar")
+            .attr("y", function (d) {
+                return y(d.name);
+            })
+            .attr("height", y.rangeBand())
+            .attr("x", 0)
+            .attr("width", function (d) {
+                return x(d.value);
+            });
+
+        //add a value label to the right of each bar
+        bars.append("text")
+            .attr("class", "label")
+            //y position of the label is halfway down the bar
+            .attr("y", function (d) {
+                return y(d.name) + y.rangeBand() / 2 + 4;
+            })
+            //x position is 3 pixels to the right of the bar
+            .attr("x", function (d) {
+                return x(d.value) + 3;
+            })
+            .text(function (d) {
+                return Math.round(d.value * 100) + '%';
+            });
+    }
+
+    function generateHalfDonut(percentage, elementId) {
+        var value = percentage
+        var text = Math.round(value * 100) + '%'
+        var data = [value, 1 - value]
+
+        // Settings
+        var width = 300
+        var height = 150
+        var anglesRange = 0.5 * Math.PI
+        var radis = Math.min(width, 2 * height) / 2
+        var thickness = 50
+        // Utility 
+        //     var colors = d3.scale.category10();
+        var colors = ["#5EBBF8", "#eaeaea"]
+
+        var pies = d3.layout.pie()
+            .value(d => d)
+            .sort(null)
+            .startAngle(anglesRange * -1)
+            .endAngle(anglesRange)
+
+        var arc = d3.svg.arc()
+            .outerRadius(radis)
+            .innerRadius(radis - thickness)
+
+        var translation = (x, y) => `translate(${x}, ${y})`
+
+        // Feel free to change or delete any of the code you see in this editor!
+        var svg = d3.select("#" + elementId).append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("class", "half-donut")
+            .append("g")
+            .attr("transform", translation(width / 2, height))
+
+
+        svg.selectAll("path")
+            .data(pies(data))
+            .enter()
+            .append("path")
+            .attr("fill", (d, i) => colors[i])
+            .attr("d", arc)
+
+        svg.append("text")
+            .text(d => text)
+            .attr("dy", "-3rem")
+            .attr("class", "label")
+            .attr("text-anchor", "middle")
+    }
+
+    $scope.GetEmployees = function () {
+        $http.post("/Home/GetAirEmployees").then(function (response) {
+            $scope.employees = response.data;
+            console.log(response.data);
+        });
+    };
+
+    $scope.GetPercentages = function () {
+        $http.post("/Home/GetPercentages").then(function (response) {
+            $scope.percentage = response.data;
+            // $scope.percntage = {"lgtbt": 0.3, "military": 0.67}
+            for (var i in $scope.percentage) {
+                generateHalfDonut($scope.percentage[i], i);
+            }
+        });
+    };
+
+    $scope.GetNumbers = function () {
+        $http.post("/Home/GetNumbers").then(function (response) {
+            $scope.number = response.data;
+            // $scope.number = {
+            //  gender: [{ name: "male", value: 0.3 }, { name: "female", value: 0.5 }],
+            //  ethnicity: [{ name: "hispanic", value: 0.3 }, { name: "black", value: 0.5 }]
+            // }
+            $timeout(function () {
+                var keys = Object.keys($scope.number);
+                for (var i = 0; i < keys.length; i++) {
+                    //if (i == "age") {
+                    //    generateBarChart($scope.number[i], i);
+                    //}
+                    generateBarChart($scope.number[keys[i]], keys[i]);
+                }
+            })
+        });
+    };
+
+    //$scope.GetGenders = function () {
+    //    $http.post("/Diversity/GetGenders").then(function (response) {
+    //        console.log(response.data);
+    //    });
+    //}
+
+    //$scope.GetLanguages = function () {
+    //    $http.post("/Diversity/GetLanguages").then(function (response)){
+    //        console.log(response.data);
+    //    }
+    //}
+
+}])
